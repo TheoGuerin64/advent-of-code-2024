da5e4c9b649074f93ea7589b0fb5a66251b85b08
Theo Guerin <theo@Theos-MacBook-Pro.local>
2024-12-04 02:49:46 +0100

    Add benchmarks to day 2

---
+++ day_2/benches/steps.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use advent_of_code_2024::{parse_input_file, step1, step2};
+
+fn bench_steps(c: &mut Criterion) {
+    let reports = parse_input_file(Path::new("input.txt")).unwrap();
+
+    let mut group = c.benchmark_group("steps");
+    group.bench_function("step1", |b| {
+        b.iter(|| step1(&reports));
+    });
+    group.bench_function("step2", |b| {
+        b.iter(|| step2(&reports));
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench_steps);
+criterion_main!(benches);
